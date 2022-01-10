7f27feea56becfe7608d2118d0bf1ae94bea93d4
ty <towyuan@outlook.com>
2022-01-10 18:09:36 +0800

    chore(types): add thenable definition

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -2,3 +2,4 @@ export * from 'ts-essentials/dist/literal-types';
 export * from 'ts-essentials/dist/types';
 export * from './types';
 export * from './mixin';
+export * from './thenable';
---
+++ packages/types/src/thenable.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/**
+ * Thenable is a common denominator between ES6 promises, Q, jquery.Deferred, WinJS.Promise,
+ * and others. This API makes no assumption about what promise library is being used which
+ * enables reusing existing code without migrating to a specific promise implementation.
+ */
+export interface Thenable<T> {
+  /**
+   * Attaches callbacks for the resolution and/or rejection of the Promise.
+   * @param onfulfilled The callback to execute when the Promise is resolved.
+   * @param onrejected The callback to execute when the Promise is rejected.
+   * @returns A Promise for the completion of which ever callback is executed.
+   */
+  then<TResult>(onfulfilled?: (value: T) => TResult | Thenable<TResult>, onrejected?: (reason: any) => TResult | Thenable<TResult>): Thenable<TResult>;
+  // eslint-disable-next-line @typescript-eslint/unified-signatures
+  then<TResult>(onfulfilled?: (value: T) => TResult | Thenable<TResult>, onrejected?: (reason: any) => void): Thenable<TResult>;
+}
