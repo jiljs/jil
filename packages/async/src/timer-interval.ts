7df64836baca02edeb9cac4f0fe2f5d8ba94d269
ty <towyuan@outlook.com>
2022-01-10 23:23:24 +0800

    chore: add more functions

---
+++ packages/async/src/timer-interval.ts
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { TimerRunner } from "./types";
+
+export class IntervalTimer {
+  protected runner?: TimerRunner;
+  private timer: any;
+
+  constructor(runner?: TimerRunner, interval?: number) {
+    this.timer = -1;
+    this.runner = runner;
+    if (typeof interval === "number") {
+      this.reschedule(interval);
+    }
+  }
+
+  static schedule(runner?: TimerRunner, interval?: number) {
+    return new IntervalTimer(runner, interval);
+  }
+
+  dispose(): void {
+    this.cancel();
+  }
+
+  cancel(): this {
+    if (this.timer !== -1) {
+      clearInterval(this.timer);
+      this.timer = -1;
+    }
+    return this;
+  }
+
+  reschedule(interval: number): void {
+    if (!this.runner) {
+      throw new Error('runner has not been set')
+    }
+    this.cancel();
+    this.timer = setInterval(() => this.runner!(), interval);
+  }
+}
+
+export function reinterval(runner?: TimerRunner, interval?: number) {
+  return IntervalTimer.schedule(runner, interval);
+}
---
+++ packages/async/src/timer-timeout.ts
@@ -0,0 +1,60 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { TimerRunner } from "./types";
+
+export class TimeoutTimer {
+  protected runner?: TimerRunner;
+  private timer: any;
+
+  constructor(runner?: () => void, timeout?: number) {
+    this.timer = -1;
+
+    if (typeof runner === 'function' && typeof timeout === 'number') {
+      this.setIfNotSet(runner, timeout);
+    }
+  }
+
+  static schedule(runner?: TimerRunner, timeout?: number) {
+    return new TimeoutTimer(runner, timeout);
+  }
+
+  dispose(): void {
+    this.cancel();
+  }
+
+  cancel(): void {
+    if (this.timer !== -1) {
+      clearTimeout(this.timer);
+      this.timer = -1;
+    }
+  }
+
+  reschedule(timeout: number) {
+    if (!this.runner) {
+      throw new Error('runner has not been set')
+    }
+    this.cancel();
+    this.timer = setTimeout(() => {
+      this.timer = -1;
+      this.runner!();
+    }, timeout);
+  }
+
+  cancelAndSet(runner: () => void, timeout: number): void {
+    this.runner = runner;
+    this.reschedule(timeout);
+  }
+
+  setIfNotSet(runner: () => void, timeout: number): void {
+    if (this.timer !== -1) {
+      // timer is already set
+      return;
+    }
+    this.runner = runner;
+    this.reschedule(timeout);
+  }
+}
+
+export function retimeout(runner?: TimerRunner, timeout?: number) {
+  return TimeoutTimer.schedule(runner, timeout);
+}
