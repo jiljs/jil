523616118a8660adab042982535969b1da060cbd
ty <towyuan@outlook.com>
2022-02-15 14:32:13 +0800

    chore: add @jil/args

---
+++ packages/args/src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * @copyright   2020, Miles Johnson
+ * @license     https://opensource.org/licenses/MIT
+ */
+
+export * from './errors';
+export * from './constants';
+export * from './format';
+export * from './parse';
+export * from './parseInContext';
+export * from './types';
+export * from './ValidationError';
---
+++ packages/args/src/utils/expandShortOption.ts
@@ -0,0 +1,17 @@
+import {ArgsError} from '../errors';
+import {AliasMap, LongOptionName, ShortOptionName} from '../types';
+
+/**
+ * Expand a short option name to a long option name.
+ */
+export function expandShortOption(short: ShortOptionName, map: AliasMap, loose: boolean): LongOptionName {
+  if (!map[short]) {
+    if (loose) {
+      return short;
+    }
+
+    throw new ArgsError('SHORT_UNKNOWN', [short]);
+  }
+
+  return map[short];
+}
